65157f6dc3482ff958a0e4789f112e4b1d77436a
Maxim <maximbelyakovs@yandex.ru>
2024-04-03 11:56:01 +0500

    autocomplete debounce

--- app/src/main/java/com/example/weather_app_belyakov/CityListActivity.kt
+++ app/src/main/java/com/example/weather_app_belyakov/CityListActivity.kt
@@ -9,11 +9,15 @@ import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weather_app_belyakov.adapter.CityAdapter
 import com.example.weather_app_belyakov.databinding.ActivityCityListBinding
 import com.example.weather_app_belyakov.model.CityResponseApi
 import com.example.weather_app_belyakov.view_model.CityViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Response
 
@@ -21,6 +25,9 @@ class CityListActivity : AppCompatActivity() {
     lateinit var binding: ActivityCityListBinding
     private val cityAdapter by lazy { CityAdapter() }
     private val cityViewModel: CityViewModel by viewModels()
+
+    private var autocompleteSearchJob: Job? = null
+    private val debouncePeriod: Long = 150
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCityListBinding.inflate(layoutInflater)
@@ -48,34 +55,43 @@ class CityListActivity : AppCompatActivity() {
 
                 override fun afterTextChanged(s: Editable?) {
                     progressBar2.visibility = View.VISIBLE
-                    cityViewModel.loadCity(s.toString(), 10)
-                        .enqueue(object : retrofit2.Callback<CityResponseApi> {
-                            override fun onResponse(
-                                call: Call<CityResponseApi>,
-                                response: Response<CityResponseApi>
-                            ) {
-                                if (response.isSuccessful) {
-                                    val data = response.body()
-                                    data?.let {
-                                        progressBar2.visibility = View.GONE
-                                        cityAdapter.differ.submitList(it as List<CityResponseApi.CityResponseApiItem>?)
-                                        cityView.apply {
-                                            layoutManager = LinearLayoutManager(
-                                                this@CityListActivity,
-                                                LinearLayoutManager.HORIZONTAL,
-                                                false
-                                            )
-                                            adapter = cityAdapter
+                    autocompleteSearchJob?.cancel()
+                    autocompleteSearchJob = lifecycleScope.launch {
+                        s?.let {
+                            delay(debouncePeriod)
+                            cityViewModel.loadCity(s.toString(), 10)
+                                .enqueue(object : retrofit2.Callback<CityResponseApi> {
+                                    override fun onResponse(
+                                        call: Call<CityResponseApi>,
+                                        response: Response<CityResponseApi>
+                                    ) {
+                                        if (response.isSuccessful) {
+                                            val data = response.body()
+                                            data?.let {
+                                                progressBar2.visibility = View.GONE
+                                                cityAdapter.differ.submitList(it as List<CityResponseApi.CityResponseApiItem>?)
+                                                cityView.apply {
+                                                    layoutManager = LinearLayoutManager(
+                                                        this@CityListActivity,
+                                                        LinearLayoutManager.HORIZONTAL,
+                                                        false
+                                                    )
+                                                    adapter = cityAdapter
+                                                }
+                                            }
                                         }
                                     }
-                                }
-                            }
 
-                            override fun onFailure(call: Call<CityResponseApi>, t: Throwable) {
+                                    override fun onFailure(
+                                        call: Call<CityResponseApi>,
+                                        t: Throwable
+                                    ) {
 
-                            }
+                                    }
+                                })
+                        }
+                    }
 
-                        })
                 }
             })
         }
